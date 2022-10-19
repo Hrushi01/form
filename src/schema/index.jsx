23da82ab7bda6668fc1761e08ec271b5deb490d4
Hrushi01 <hrushikesh.ambike@gmail.com>
2022-10-19 13:17:51 +0530

    phone validation updated

--- src/schema/index.jsx
+++ src/schema/index.jsx
@@ -1,13 +1,17 @@
 import * as Yup from "yup";
 import "yup-phone";
 
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(25).required("Please enter your name"),
   email: Yup.string().email().required("Please enter your email"),
 
   phone: Yup.string()
-    .phone()
-    .required(null, true, "Please enter correct mobile number"),
+    .matches(phoneRegExp, "Phone number is not valid")
+    .max(10, "Phone number is not valid")
+    .min(10, "Phone number is not valid"),
   gender: Yup.string().required("Please select one"),
   git: Yup.string()
     .min(5, "mininum length 5")
